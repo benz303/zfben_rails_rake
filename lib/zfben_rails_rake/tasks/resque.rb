0c5cdd05a9d3cb8ec6e7f2344292c267f31b7100
ben <ben@zfben.(none)>
2011-11-02 17:26:04 +0800

    v0.0.17

--- lib/zfben_rails_rake/tasks/resque.rb
+++ lib/zfben_rails_rake/tasks/resque.rb
@@ -1,44 +1,53 @@
 if File.exists?(ROOT + '/config/initializers/resque.rb')
+  require ROOT + '/config/initializers/resque.rb'
+  require 'resque/tasks'
+  if defined?(Resque::Scheduler)
+    require 'resque_scheduler/tasks'
+  end
   namespace :resque do
-    desc 'Start Resque for debug'
-    task :debug => :environment do
-      require ROOT + '/config/initializers/resque.rb'
-      worker = Resque::Worker.new('*')
-      worker.work
+    task :setup => :environment
+
+    task :_work => ['resque:preload', 'resque:setup'] do
+      work = Resque::Worker.new('*')
+      Process.daemon(true)
+      File.open(ROOT + '/tmp/resque.pid', 'w') { |f| f << work.pid }
+      work.work(5)
+    end
+
+    if defined?(Resque::Scheduler)
+      task :_scheduler => 'resque:setup' do
+        Process.daemon(true)
+        File.open(ROOT + '/tmp/scheduler.pid', 'w') { |f| f << Process.pid.to_s }
+        Resque::Scheduler.run
+      end
     end
 
     desc 'Start Resque daemon worker'
     task :start => 'resque:stop' do
-      RAILS_ENV = 'production'
-      Rake::Task[:environment].execute
-      work = fork do
-        worker = Resque::Worker.new('*')
-        Process.daemon true
-        File.open(ROOT + '/tmp/resque.pid', 'w') { |f| f << Process.pid.to_s }
-        worker.work
+      sys "bash -c 'RAILS_ENV=production rake resque:_work'"
+      if defined?(Resque::Scheduler)
+        sys "bash -c 'RAILS_ENV=production rake resque:_scheduler'"
       end
-      print "Resque worker running at #{work}\n"
     end
 
     desc 'Stop Resque worker'
     task :stop do
-      path = ROOT + '/tmp/resque.pid'
-      if File.exists?(path)
-        sys "kill `cat #{path}`;rm #{path}"
+      ['resque', 'scheduler'].each do |name|
+        path = ROOT + '/tmp/' + name + '.pid'
+        if File.exists?(path)
+          sys "kill `cat #{path}`;rm #{path}"
+        end
       end
     end
 
     desc 'Clear Resque data'
     task :clear do
-      require ROOT + '/config/initializers/resque.rb'
       Resque.redis.keys('*').each{ |k| Resque.redis.del k }
     end
 
     desc 'Start Resque web interface'
     task :web do
-      require ROOT + '/config/initializers/resque.rb'
-      require 'resque/server'
-      Resque::Server.run!
+      sys "RAILS_ENV=production resque-web #{ROOT}/config/initializers/resque.rb"
     end
   end
 end
