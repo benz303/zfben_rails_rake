2da0a01b0ad7d2e0a13a0e7a09c1387b4e79e3f7
ben <ben@ben-ubuntu.(none)>
2012-11-20 17:34:27 +0800

    update mongoid

--- lib/zfben_rails_rake/tasks/mongodb.rb
+++ lib/zfben_rails_rake/tasks/mongodb.rb
@@ -1,12 +1,14 @@
 if File.exist? Rails.root.join('config/mongoid.yml')
   namespace :mongodb do
-    config = YAML.load(File.read(Rails.root.join('config/mongoid.yml')))
+    config = YAML.load(File.read(Rails.root.join('config/mongoid.yml')))[Rails.env.to_s]['sessions']['default']['hosts'][0]
+    host = config.split(':')[0]
+    port = config.split(':')[1]
 
     desc 'Start Mongodb'
     task :start do
       zfben_rails_rake_system "mkdir #{Rails.root}/mongo" unless File.exists?(Rails.root.join('mongo'))
       zfben_rails_rake_system "mkdir #{Rails.root}/log" unless File.exists?(Rails.root.join('log'))
-      zfben_rails_rake_system "mongod --nohttpinterface --nojournal --port #{config['production']['port']} --bind_ip #{config['production']['host']} --dbpath #{Rails.root}/mongo --fork --logpath #{Rails.root}/log/mongodb.log"
+      zfben_rails_rake_system "mongod --nohttpinterface --nojournal --port #{port} --bind_ip #{host} --dbpath #{Rails.root}/mongo --fork --logpath #{Rails.root}/log/mongodb.log"
     end
 
     desc 'Stop Mongodb'
@@ -23,13 +25,13 @@ if File.exist? Rails.root.join('config/mongoid.yml')
       zfben_rails_rake_system 'rm -r mongo/*'
     end
     
-    backup = "mongodump --host #{config['production']['host']} --port #{config['production']['port']}"
+    backup = "mongodump --host #{host} --port #{port}"
     desc backup
     task :dump do
       zfben_rails_rake_system backup
     end
 
-    restore = "mongorestore --host #{config['production']['host']} --port #{config['production']['port']}"
+    restore = "mongorestore --host #{host} --port #{port}"
     desc restore
     task :restore do
       zfben_rails_rake_system restore
@@ -37,7 +39,7 @@ if File.exist? Rails.root.join('config/mongoid.yml')
     
     desc 'Repair Mongodb'
     task :repair do
-      "mongod --nohttpinterface --nojournal --port #{config['production']['port']} --bind_ip #{config['production']['host']} --dbpath #{Rails.root}/mongo --repair"
+      "mongod --nohttpinterface --nojournal --port #{port} --bind_ip #{host} --dbpath #{Rails.root}/mongo --repair"
     end
   end
 end
--- zfben_rails_rake.gemspec
+++ zfben_rails_rake.gemspec
@@ -3,21 +3,13 @@ $:.push File.expand_path("../lib", __FILE__)
 
 Gem::Specification.new do |s|
   s.name        = 'zfben_rails_rake'
-  s.version     = '0.0.22'
+  s.version     = '0.0.23'
   s.platform    = Gem::Platform::RUBY
   s.authors     = ['Ben']
   s.email       = ['ben@zfben.com']
   s.homepage    = 'https://github.com/benz303/zfben_rails_rake'
   s.summary     = %q{}
   s.description = %q{}
-  
-  s.post_install_message =%q{********************************************************************************
-  Thank you for using zfben_rails_rake!
-  
-  Please follow @zfben on Twitter for announcements, updates, and news.
-  https://twitter.com/zfben
-********************************************************************************
-}
 
   s.files         = `git ls-files`.split("\n")
   s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
